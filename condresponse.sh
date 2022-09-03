a7d2e4613bc2841ff71274c1838adb85802e0b68
eneroh <28052446+eneroh@users.noreply.github.com>
2022-09-04 08:59:37 +1000

    Update condresponse.sh

--- condresponse.sh
+++ condresponse.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+#conditional response, you have to meet the previous coniditions for the reply "It's alive" to appear
 
 ping -c 1 raspberrypi.snopoint.com | grep "time" && echo "It's alive"
