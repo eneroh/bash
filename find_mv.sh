10de9bf8c32af645721216db982ec4138781cb3e
eneroh <28052446+eneroh@users.noreply.github.com>
2025-01-25 17:43:04 +1100

    Create find_mv.sh

---
+++ find_mv.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# find_mv.sh
+#
+# Got a bit annoyed that my find function wasn't able to move things easily, so now I'm creating this script to make this process smoother
+# Features:
+# - User input a term search
+# - Confirmation move
+# - clear variables
+# - cp instead of mv safety (moved a bunch of test files from /home/<user> and filled my test folder with junk T_T
+
+echo "Input original file/s directory [/home/<user>/<dir>]: "
+read origdir
+echo "Input search term: "
+read input
+echo "Input destination directory: [/home/<user>/<dir>]"
+read destdir
+echo "Is this information correct? '$origdir $input $destdir' [Y/n]: "
+read runMain
+if [[ "$runMain" == [Yy] ]]; then
+	echo "Success!"
+	find $origdir -type f -name "*$input*" -exec cp -t $destdir {} +
+else
+	echo "Please enter values!"
+fi
+
+# What do the {} and + mean?
+# {} replace the {} with each find reult in turn
+# + limits the amount of times the mv function is run
+
+# failed attempts
+# find /home/en/Downloads -type f -name "*<text>*" -exec mv -t /home/<user>/Downloads/<text> {} +
+
+# mv $(find /home/<user>/Downloads -name "*<text>*") /home/<user>/Downloads/<text>
+
+# for f in `find /home/<user>/Downloads -name "*<text>*"`; do cp $f /home/<user>/Downloads/<text>/; done
+# source: https://unix.stackexchange.com/questions/154818/how-to-integrate-mv-command-after-find-command
