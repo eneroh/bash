95cf5a7f2f13379d2110e65883cbde254cdbdaf3
eneroh <28052446+eneroh@users.noreply.github.com>
2025-01-26 11:28:33 +1100

    Create find_mv_v2.sh

---
+++ find_mv_v2.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+### find_mv_v2.sh ###
+# update to original but with confirmation of files before executing the copy/move, specifically an output of what you are about to move. This functionality wasn't present before
+# More additions to come:
+# - 
+
+echo "Input original file/s directory [/home/<user>/<dir>]: "
+read origdir
+echo "Input search term: "
+read teststring
+echo "Input destination directory: [/home/<user>/<dir>]: "
+read destdir
+echo "Is this information correct? [Y/n]: "
+read resp1
+if [[ "$resp1" == [Yy] ]]; then
+	echo "Success!"
+	output=$(find $origdir -type f -name "*$teststring*" > test.txt >&2)
+	#readfile=$(cat test.txt | nl)
+	echo $readfile
+	echo "Would you like to move these files? [Y/n]: "
+	read resp2
+	if [[ "$resp2" == [Yy] ]]; then
+		find $origdir -type f -name "*$teststring*" -exec cp -t $destdir {} +
+	else
+		echo "Exiting"
+		break
+	fi
+else
+	echo "Please enter values!"
+fi
