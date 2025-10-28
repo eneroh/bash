78814fb46c2a0a9a6d9c11fcad08ba98d81ab2ca
eneroh <28052446+eneroh@users.noreply.github.com>
2025-10-28 20:58:46 +1100

    Update clearLogs.sh

--- clearLogs.sh
+++ clearLogs.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+### Pending upgrades ###
+# - Load relevant dependencies
+# - Create a temporary backup directory backup/tmp_$(date +%F)
+# - Pause or flush services
+# - Create data archive using tar/rync
+# - Verify backup integrity with sha256sum 
+# - Upload to remote storage i.e. cloud services/offsite location and confirm success w/ logs present
+# - Rotate old backups
+# - Delete local and remote backups older than retention period
+# - Restart
+# - Clean up logs older than 10 days
+###
+
 LOG_DIR="/var/log"
 PRUNE_DAYS=10
 
