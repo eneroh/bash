8eb15041546fbffb1b26ee6ca13400257a8b75c1
eneroh <28052446+eneroh@users.noreply.github.com>
2026-07-25 15:36:40 +1000

    Create moveFilev3.sh

---
+++ moveFilev3.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+vpnConf=$(mullvad status | grep -i "Connected")
+
+if [[ "$vpnConf" == "Connected" ]]; then
+  printf "%b" "Do you want to turn off your VPN? [Y/n]: "
+  read -rn1 -- vpnInput
+  if [[ "$vpnInput" == [Yy] ]]; then
+    mullvad disconnect
+    printf "%b" "\nInput movie to move: "
+    read -r -- input
+    fileSel=$(ls | grep -i $input | fzf)
+
+    printf "%b" "1.movies\n2.tv\n3.music\n"
+    printf "%s" "Input your category: "
+    read -rn1 -- location
+
+    case $location in
+	1) loc=$(printf "/mass/movies") ;;
+	2) loc=$(printf "/mass/tv") ;;
+	3) loc=$(printf "/media") ;;
+	*) exit; ;;
+    esac
+
+    sudo rsync -rRP "$fileSel" anon@ubuntusrv:$loc 
+  else
+    printf "%b" "\nVPN is not disconnected, please try again later!\n"
+  fi
+else
+  printf "%b" "\nInput media file to move: "
+  read -r -- input
+  fileSel=$(ls | grep -i $input | fzf)
+
+  printf "%b" "1.movies\n2.tv\n3.music\n"
+  printf "%b" "\nInput your category: "
+  read -rn1 -- location
+
+  case $location in
+	1) loc=$(printf "/mass/movies") ;;
+	2) loc=$(printf "/mass/tv") ;;
+	3) loc=$(printf "/media") ;;
+	*) exit; ;;
+  esac
+
+  sudo rsync -rRP "$fileSel" anon@ubuntusrv:$loc   
+fi
+
+printf "%b" "\n$fileSel has been successfully moved to server\n" >> ./moveFiles_history.txt
