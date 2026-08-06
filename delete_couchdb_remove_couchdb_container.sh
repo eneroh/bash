6eca69e78bfd7ca7c43d6743a0aa09ffaffa4c7a
eneroh <28052446+eneroh@users.noreply.github.com>
2026-08-06 10:07:46 +1000

    Update delete_couchdb_remove_couchdb_container.sh

--- delete_couchdb_remove_couchdb_container.sh
+++ delete_couchdb_remove_couchdb_container.sh
@@ -8,31 +8,31 @@ if [[ "$wipeInput" == [Yy] ]]; then
   printf "%b" "\nYou have selected to completely start fresh with your couchdb database\n"
   printf "%b" "Wiping couchdb directories: /opt/couchdb\n"
   sudo rm -rf /opt/couchdb
-  printf "%b" "Do you want to remove your couchdb container? [y/N]: "
+  printf "%b" "Do you want to remove your container? [y/N]: "
   read -rn1 -- input
   if [[ "$input" == [Yy] ]]; then
-    cName=$(sudo docker ps | awk '{print $1}' | sed 's/CONTAINER//g' | tr -d '\n')
-    printf "%b" "\nYou have selected to remove couchdb docker container instance: $cName\n"
-    printf "%b" "Stopping couchdb instance: $cName\n"
+    cName=$(docker ps | awk '{print $1}' | sed 's/CONTAINER//g' | fzf)
+    printf "%b" "\nYou have selected to remove docker container instance: $cName\n"
+    printf "%b" "Stopping docker instance: $cName\n"
     sudo docker stop "$cName"
-    printf "%b" "Removing couchdb instance: $cName\n"
+    printf "%b" "Removing docker instance: $cName\n"
     sudo docker rm "$cName"
     sudo docker ps
   else
-    printf "%b" "\nYou have chosen to *NOT* delete your couchdb container. \nCancelling operation.\n"
+    printf "%b" "\nYou have chosen to *NOT* delete your docker container. \nCancelling operation.\n"
   fi
 else	
-  printf "%b" "Do you want to remove your couchdb container? [y/N]: "
+  printf "%b" "\nDo you want to remove your container? [y/N]: "
   read -rn1 -- input
   if [[ "$input" == [Yy] ]]; then
-    cName=$(sudo docker ps | awk '{print $1}' | sed 's/CONTAINER//g' | tr -d '\n')
-    printf "%b" "\nYou have selected to remove couchdb docker container instance: $cName\n"
+    cName=$(docker ps | awk '{print $1}' | sed 's/CONTAINER//g' | fzf)
+    printf "%b" "\nYou have selected to remove docker container instance: $cName\n"
     printf "%b" "Stopping couchdb instance: $cName\n"
     sudo docker stop "$cName"
     printf "%b" "Removing couchdb instance: $cName\n"
     sudo docker rm "$cName"
     sudo docker ps
   else
-    printf "%b" "\nYou have chosen to not delete your couchdb container. \nCancelling operation.\n"
+    printf "%b" "\nYou have chosen to not delete your docker container. \nCancelling operation.\n"
   fi
 fi
