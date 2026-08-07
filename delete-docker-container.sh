68566ffb85ce883a3176ff7da9e14a8f0d34fc5f
eneroh <28052446+eneroh@users.noreply.github.com>
2026-08-07 17:49:13 +1000

    Update delete-docker-container.sh

--- delete-docker-container.sh
+++ delete-docker-container.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 #
 
-printf "%b" "Would you like to wipe your couchdb instance entirely? [y/N]: "
+printf "%b" "Would you like to wipe your existing files? [y/N]: "
 read -rn1 -- wipeInput
+printf "%" "Input file directory to delete: /opt/couchdb or /data/"
+read -r -- dirInput
 if [[ "$wipeInput" == [Yy] ]]; then
   printf "%b" "\nSudo will be used to wipe relevant folders"
   printf "%b" "\nYou have selected to completely start fresh with your couchdb database\n"
   printf "%b" "Wiping couchdb directories: /opt/couchdb\n"
-  sudo rm -rf /opt/couchdb
+  sudo rm -rf $(dirInput)
   printf "%b" "Do you want to remove your container? [y/N]: "
   read -rn1 -- input
   if [[ "$input" == [Yy] ]]; then
-    cName=$(docker ps | awk '{print $1}' | sed 's/CONTAINER//g' | tail -n 2 | fzf)
+    cName=$(docker ps | awk '{print $1}' | tail -n 2 | fzf)
     printf "%b" "\nYou have selected to remove docker container instance: $cName\n"
     printf "%b" "Stopping docker instance: $cName\n"
     sudo docker stop "$cName"
@@ -25,7 +27,7 @@ else
   printf "%b" "\nDo you want to remove your container? [y/N]: "
   read -rn1 -- input
   if [[ "$input" == [Yy] ]]; then
-    cName=$(docker ps | awk '{print $1}' | sed 's/CONTAINER//g' | tail -n 2 |  fzf)
+    cName=$(docker ps | awk '{print $1}' | tail -n 2 |  fzf)
     printf "%b" "\nYou have selected to remove docker container instance: $cName\n"
     printf "%b" "Stopping couchdb instance: $cName\n"
     sudo docker stop "$cName"
